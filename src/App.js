51a82eefae10a0fcd440d0de6bfdfa628e5ca3cd
kevinrichard31 <kevinrichard31@gmail.com>
2024-12-13 22:33:28 +0100

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Heroscene from './components/Heroscene/heroscene';
+
+function App() {
+  return (
+    <div className="App">
+        <Heroscene/>
+      
+    </div>
+  );
+}
+
+export default App;
