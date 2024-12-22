8c26c94a12ea15478b5a17e0c6823e9ca1d46ff2
kevin <kevinrichard31@gmail.com>
2024-12-22 22:27:16 +0100

    Main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Contact from './components/Contact/contact';
+import Header from './components/Header/header';
 import Heroscene from './components/Heroscene/heroscene';
+import Projects from './components/Projects/projects';
+import { useRef } from 'react';
 
 function App() {
+  const projectsRef = useRef(null);
   return (
     <div className="App">
+        <Header projectsRef={projectsRef}/>
         <Heroscene/>
-      
+        <Projects ref={projectsRef}/>
+        <Contact />
     </div>
   );
 }
--- src/components/Heroscene/heroscene.js
+++ src/components/Heroscene/heroscene.js
@@ -4,15 +4,19 @@ import './style.css'
 function Heroscene() {
     return (
 
-        <div class="hero bg-base-200 min-h-screen">
-            <div class="hero-content flex-col lg:flex-row">
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    class="max-w-sm rounded-lg shadow-2xl" />
+                    className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Kevin Spider</h1>
-                    <p class="py-6">
-                        Le meilleur développeur de tout les temps
+                <div className="flex flex-col">
+                    <div className="platypi-500 text-2xl md:text-3xl">Kevin Richard</div>
+
+                    <div className="hidden md:block">Développeur FrontEnd React / Typescript</div>
+                </div>
+                    <p className="py-6">
+                        META CERTIFIED | in progress
                     </p>
                 </div>
             </div>
